e5d8d3c83baadab09f905abe003b4e3b28dfed9e
sho.fuchiwaki <s.fuchiwaki.edupress@gmail.com>
2021-09-09 09:52:46 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,8 +44,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        // メッセージを作成
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
+        // タスクを作成
         $task = new Task;
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
@@ -96,9 +102,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
