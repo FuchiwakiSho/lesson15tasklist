f5088b162eba3f9d73bfb18934fa9cbfbbc01742
sho.fuchiwaki <s.fuchiwaki.edupress@gmail.com>
2021-09-20 11:50:32 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,18 +2,20 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>TaskBoard</title>
+        <title>Microposts</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     </head>
 
     <body>
+
         {{-- ナビゲーションバー --}}
         @include('commons.navbar')
 
         <div class="container">
             {{-- エラーメッセージ --}}
             @include('commons.error_tasks')
+
             @yield('content')
         </div>
 
